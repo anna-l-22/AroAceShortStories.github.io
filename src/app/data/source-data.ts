0d1d868265f437ea1c4a69c30b9ca2071a7dcd02
anna-l-22 <161951896+anna-l-22@users.noreply.github.com>
2024-03-02 10:50:58 -0500

    Update source-data.ts for common bonds

--- src/app/data/source-data.ts
+++ src/app/data/source-data.ts
@@ -8,5 +8,8 @@ export const SOURCE: DataSource[] = [
     { id: 3, source: "Bones of Green and Hearts of Gold", storygraph: "https://app.thestorygraph.com/books/9db4f433-6685-4c22-b472-d9e8e63c7f7a", 
         goodreads: "https://www.goodreads.com/book/show/55886630-bones-of-green-and-hearts-of-gold", author: "https://aroworlds.com/fiction/collections/bones-of-green-and-hearts-of-gold/"},
     { id: 4, source: "Hallo, Aro", goodreads: "https://www.goodreads.com/book/show/201381133-hallo-aro", 
-        author: "https://aroworlds.com/fiction/collections/hallo-aro/"}
+        author: "https://aroworlds.com/fiction/collections/hallo-aro/"},
+    { id: 5, source: "Common Bonds: An Aromantic Speculative Anthology", goodreads: "https://www.goodreads.com/en/book/show/51563160", 
+        storygraph: "https://app.thestorygraph.com/books/eb99d06b-c9be-4d34-a22c-21eb421af84b", author: "https://claudiearseneault.com/books/common-bonds/"}
+
 ]
