66190e05cf31f1cfb3c5f474e1a5f58b8a0a4b2d
anna-l-22 <161951896+anna-l-22@users.noreply.github.com>
2024-05-04 00:19:55 -0400

    Update series-data.ts
    Updated for being ace

--- src/app/data/series-data.ts
+++ src/app/data/series-data.ts
@@ -7,5 +7,7 @@ export const SERIES: DataSeries[] = [
     goodreads: "https://www.goodreads.com/series/205952-chameleon-moon", author: "https://www.roannasylver.com/books"},
     { id: 3, series: "Rowan Ross’s Guide to Aromanticism", author: "https://aroworlds.com/fiction/rowan-rosss-guide-to-aromanticism/"},
     { id: 4, series: "Marchverse", author: "https://aroworlds.com/marchverse/"},
-    { id: 5, series: "Elisa"}
+    { id: 5, series: "Elisa"},
+    { id: 12, source: "Being Ace", goodreads: "https://www.goodreads.com/book/show/65212707-being-ace?ref=nav_sb_ss_1_9",
+    storygraph: "https://app.thestorygraph.com/books/fba9cf0e-5bba-4f15-a221-fafca93a08fe", author: "http://madelinedyer.co.uk/being-ace/"}
 ]
